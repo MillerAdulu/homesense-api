10ddcdbacbae3391376d74c9a1cbda3b03c67bd2
Miller Adulu <milleradulu@gmail.com>
2019-03-15 11:52:02 +0300

    Fix prod.exs deploy error

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :homesenseapi, HomesenseapiWeb.Endpoint,
   url: [scheme: "https", host: "homesenseapi.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
