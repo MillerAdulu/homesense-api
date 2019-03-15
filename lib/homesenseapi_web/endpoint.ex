bb16663c3637814d828c77c55e7989d314a82325
Miller Adulu <milleradulu@gmail.com>
2019-03-15 11:50:10 +0300

    Configure Heroku deploy settings

--- lib/homesenseapi_web/endpoint.ex
+++ lib/homesenseapi_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule HomesenseapiWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :homesenseapi
 
   socket "/socket", HomesenseapiWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
